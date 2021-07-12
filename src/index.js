fcd2998a36caa9c5dec66cb7d25d8c339a814d7a
Vyacheslav Kotelnikov <kotelnikovvv@msk.bcs.ru>
2021-07-12 03:00:30 +0300

    optimizations

--- src/index.js
+++ src/index.js
@@ -6,112 +6,160 @@ import pizzaImg from './index.jpeg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Form from 'react-bootstrap/Form'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Legend() {
-  return (
-    <div>
-        <Row/>
-        <Row>
-            Цена
+  const labels = [null, 'Цена', 'Радиус', 'Размер', 'Площадь', 'Цена/кв.см',];
+  return labels.map(function(label, index) {
+      return <Row key={index} className="justify-content-right">
+          <Col align="center">
+            {label}
+          </Col>
         </Row>
-        <Row>
-            Размер
-        </Row>
-        <Row>
-            Радиус
-        </Row>
-        <Row>
-            Цена/кв.см.
-        </Row>
-        <Row>
-            Площадь
-        </Row>
-      </div>
-  )
+  });
+}
+
+class PizzaCardRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pizzaCards: [
+        {
+          price: 500,
+          radius: 25,
+          maxRadius: 40,
+          style: null,
+        },
+        {
+          price: 500,
+          radius: 25,
+          maxRadius: 40,
+          style: null,
+        },
+      ]
+    };
+
+    this.handlePriceChange = this.handlePriceChange.bind(this);
+    this.handleRadiusChange = this.handleRadiusChange.bind(this);
+  }
+
+  handlePriceChange(newPrice, index) {
+    let newPizzaCards = this.state.pizzaCards.slice();
+    newPizzaCards[index].price = newPrice;
+    this.setState({pizzaCards: newPizzaCards});
+  }
+
+  handleRadiusChange(newRadius, index) {
+    let newPizzaCards = this.state.pizzaCards.slice();
+    newPizzaCards[index].radius = newRadius;
+    this.setState({pizzaCards: newPizzaCards});
+  }
+
+  getPricePerCmAndSquare(pizza) {
+    let square = Math.round(Math.PI * Math.pow(pizza.radius / 2, 2));
+    let pricePerSquareCm = (pizza.price / square).toPrecision(2);
+    return {
+      pricePerSquareCm: pricePerSquareCm,
+      square: square,
+    }
+  };
+
+  render() {
+    let params = [this.getPricePerCmAndSquare(this.state.pizzaCards[0]), this.getPricePerCmAndSquare(this.state.pizzaCards[1]), ];
+
+    let styles=[{},{}];
+    
+    if (params[0].pricePerSquareCm > params[1].pricePerSquareCm) {
+      let ratio = Math.pow(0.6, 2) * (100 - (params[1].pricePerSquareCm * 100 / params[0].pricePerSquareCm)) / 100;
+    
+      // console.log('red ratio left',ratio);
+      styles[0].style = {
+        backgroundImage: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0), rgba(255,0,0,' + (ratio) + '))',
+      };
+      styles[1].style = {
+        backgroundImage: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0), rgba(0,255,0,' + (ratio) + '))',
+      };
+    } else {
+      let ratio = Math.pow(0.6, 2) * (100 - (params[0].pricePerSquareCm * 100 / params[1].pricePerSquareCm)) / 100;
+      // console.log('red ratio right',ratio);
+      styles[0].style = {
+        backgroundImage: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0), rgba(0,255,0,' + (ratio) + '))',
+      };
+      styles[1].style = {
+        backgroundImage: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0),rgba(0,0,0,0), rgba(255,0,0,' + (ratio) + '))',
+      };
+    }
+
+
+    let pizzas = this.state.pizzaCards.map((pizza, index) => {
+
+      return (
+      <Col key={index} style={styles[index].style}>
+          <PizzaCard id={index} price={this.state.pizzaCards[index].price} radius={this.state.pizzaCards[index].radius} square={params[index].square} pricePerSquareCm={params[index].pricePerSquareCm} maxRadius={pizza.maxRadius} onPriceChange={this.handlePriceChange} onRadiusChange={this.handleRadiusChange}/>
+      </Col>);
+    });
+
+    return pizzas;
+  }
 }
 
 class PizzaCard extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      price: 500,
-      radius: 25,
-      maxRadius: 40,
-    }
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handlePriceChange = this.handlePriceChange.bind(this);
     this.handleRadiusChange = this.handleRadiusChange.bind(this);
   }
 
-
-  calcPizza = () => {
-    // console.log(this.pizza.nativeElement);
-    this.pizzaScale = this.sliderValue / this.sliderMaxValue;
-    this.refreshSquareValueOnSliderMove();
-    this.refreshPricePerCm();
-  };
-
-  handleChange(e) {
-    // if(e.type === "input") {
-      // this.calcPizza();
-      this.setState({price: e.target.value});
-    // }
+  handlePriceChange(e) {
+    this.props.onPriceChange(e.target.value, this.props.id);
   }
 
   handleRadiusChange(e) {
-    this.setState({radius: e.target.value});
+    this.props.onRadiusChange(e.target.value, this.props.id);
   }
 
   render() {
-    let square = Math.round(Math.PI * Math.pow(this.state.radius / 2, 2));
-    let pricePerSquareCm = (this.state.price / square).toPrecision(2);
-
-    const pizzaScale = this.state.radius / this.state.maxRadius;
+    const pizzaScale = this.props.radius / this.props.maxRadius;
     let pizzaStyle = {
       transform: "scale(" + pizzaScale + ")"
     }
-    return(
-      <div>
-        <Row className="justify-content-center">
-            <img src={pizzaImg} style={pizzaStyle} id="pizzaImage" alt="pizzaImage"/>
-        </Row>
-        <Row className="justify-content-center">
-            <input type="number" id="price" value={this.state.price} onChange={this.handleChange}/>
-        </Row>
-        <Row className="justify-content-center">
-            <input id="slider" style={{width:"100%"}} type="range" value={this.state.radius} onChange={this.handleRadiusChange} min="15" max={this.state.maxRadius} />
-        </Row>
-        <Row className="justify-content-center">
-            {this.state.radius} см
-        </Row>
-        <Row className="justify-content-center">
-            {pricePerSquareCm}
-        </Row>
-        <Row className="justify-content-center">
-            {square}
-        </Row>
-      </div>
-    );
+
+    let items = [
+      <Image fluid src={pizzaImg} style={pizzaStyle} id="pizzaImage" alt="pizzaImage"/>,
+      <Form.Control type="number" id="price" value={this.props.price} onChange={this.handlePriceChange}/>,
+      this.props.radius + ' см',
+      <Form.Control id="slider" type="range" value={this.props.radius} onChange={this.handleRadiusChange} min="15" max={this.props.maxRadius}/>,
+      this.props.square,
+      this.props.pricePerSquareCm
+    ]
+
+  return items.map(function(item, index) {
+    return (
+      <Row key={index} noGutters>
+        <Col align="center">
+            {item}
+        </Col>
+      </Row>
+    )
+  });
+
   }
 }
 
 
 ReactDOM.render(
-  <Container fluid="md">
+  <Container fluid="sm">
     <React.StrictMode>
-      <Row md="auto" className="flex-nowrap justify-content-center align-items-end">
-        <Col md="2">
+      <Row className="flex-nowrap justify-content-center align-items-end">
+        <Col xs="4" >
           <Legend />
         </Col>
-        <Col md="auto">
-          <PizzaCard />
-        </Col>
-        <Col md="auto">
-          <PizzaCard />
-        </Col>
+        <PizzaCardRow />
       </Row>
     </React.StrictMode>
   </Container>,
